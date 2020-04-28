de77c07303937d56b61449f57c7161fe08093a7c
VenkateshAnbazhagan <venkatesh.anbazhagan@worldvision.org.uk>
2020-04-28 19:14:32 +0530

    ServiceFabricPOC

---
+++ Service1/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service1.Controllers
+{
+    class WebApp1
+    {
+        public String Name { get; set; }
+        public int ID { get; set; }
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        List<WebApp1> weblist = new List<WebApp1>();
+        public IActionResult Index()
+        {
+            weblist.Add(new WebApp1() { ID = 1, Name = "Web1" });
+            return new JsonResult(weblist);
+        }
+    }
+}
---
+++ Service4/Controllers/WebController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1Service.Controllers
+{
+    class WebApp1
+    {
+        public String Name { get; set; }
+        public int ID { get; set; }
+    }
+    [Route("api/[controller]")]
+    public class WebController : Controller
+    {
+        List<WebApp1> weblist = new List<WebApp1>();
+        public IActionResult Index()
+        {
+            weblist.Add(new WebApp1() { ID=1, Name = "Web1"});
+            return new JsonResult(weblist);
+         }
+    }
+}
